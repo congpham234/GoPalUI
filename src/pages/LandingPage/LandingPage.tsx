5561c3a53d2c64c4f3621534db3e5510591a892f
Cong pham <>
2024-05-05 18:27:29 -0700

    Add prettify and eslint

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -6,30 +6,30 @@ import Typography from '../../components/Typography';
 
 function LandingPage() {
     return (
-      <div className="LandingPage">
-        <div>
-            <Typography variant='h1'> Heading 1 </Typography>
-            <Typography variant='h2'> Heading 2 </Typography>
-            <Typography variant='h3'> Heading 3 </Typography>
-            <Typography variant='headline'> Headline </Typography>
-            <Typography variant='bodyBold'> Body Bold </Typography>
-            <Typography variant='body'> Body </Typography>
-            <Typography variant='subHead'> Subhead </Typography>
-            <Typography variant='caption'> Caption </Typography>
+        <div className="LandingPage">
+            <div>
+                <Typography variant='h1'> Heading 1 </Typography>
+                <Typography variant='h2'> Heading 2 </Typography>
+                <Typography variant='h3'> Heading 3 </Typography>
+                <Typography variant='headline'> Headline </Typography>
+                <Typography variant='bodyBold'> Body Bold </Typography>
+                <Typography variant='body'> Body </Typography>
+                <Typography variant='subHead'> Subhead </Typography>
+                <Typography variant='caption'> Caption </Typography>
+            </div>
+            <div>
+                <CustomButton customVariant="primary" onClick={() => console.log('Button clicked')}><Typography variant="body">
+                    Primary Button</Typography>
+                </CustomButton>
+                <CustomButton customVariant="secondary" onClick={() => console.log('Button clicked')}>
+                    Secondary Button
+                </CustomButton>
+                <CustomButton customVariant="tertiary" onClick={() => console.log('Button clicked')}>
+                    Tertiary Button
+                </CustomButton>
+            </div>
         </div>
-        <div>
-            <CustomButton customVariant="primary" onClick={() => console.log('Button clicked')}><Typography variant="body">
-                Primary Button</Typography>
-            </CustomButton>
-            <CustomButton customVariant="secondary" onClick={() => console.log('Button clicked')}>
-                Secondary Button
-            </CustomButton>
-            <CustomButton customVariant="tertiary" onClick={() => console.log('Button clicked')}>
-                Tertiary Button
-            </CustomButton>
-        </div>
-      </div>
     );
-  }
-  
-  export default LandingPage;
+}
+
+export default LandingPage;
