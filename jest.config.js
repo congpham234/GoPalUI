83ad63f9d4188e5979f6900690b52402b4983879
Cong pham <>
2024-05-05 18:34:47 -0700

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,14 @@
 module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
+  transform: { '^.+\\.ts?$': 'ts-jest' },
   testEnvironment: 'node',
   testRegex: '/tst/.*\\.(test|spec)?\\.(ts)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverage: true,
   collectCoverageFrom: [
-    '!**/*.{js,jsx}',
     '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/app.ts',
-    '!**/src/mocks/**',
-    '!**/src/models/**',
-    '!**/routes/router.ts',
-    '!**/apis/**',
-    '!**/generated/**',
-    '!**/middlewares/**',
-    '!**/exceptions/**',
-    '!**/routes/**',
+    '!**/build/**',
   ],
-  coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
+  coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
   // coverageThreshold: {
   //   global: {
   //     functions: 90,
